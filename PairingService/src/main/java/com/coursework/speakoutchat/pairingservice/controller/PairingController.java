b1b285d467cd4c553ff92b3c66eb39a1640f86ac
Danylo Andrieiev <danandryeyev@gmail.com>
2023-05-19 00:07:52 +0300

    Fix PairingController

--- PairingService/src/main/java/com/coursework/speakoutchat/pairingservice/controller/PairingController.java
+++ PairingService/src/main/java/com/coursework/speakoutchat/pairingservice/controller/PairingController.java
@@ -1,6 +1,5 @@
 package com.coursework.speakoutchat.pairingservice.controller;
 
-import com.coursework.speakoutchat.pairingservice.data.SessionUserTopic;
 import com.coursework.speakoutchat.pairingservice.data.UserPair;
 import com.coursework.speakoutchat.pairingservice.data.UserTopic;
 import com.coursework.speakoutchat.pairingservice.service.PairingService;
@@ -10,8 +9,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.*;
 
 @Controller
 public class PairingController {
@@ -26,19 +23,13 @@ public class PairingController {
         this.pairingService = pairingService;
     }
 
-    private final List<SessionUserTopic> sessionUserTopics = Collections.synchronizedList(new ArrayList<>());
-
     @MessageMapping("/pair")
     public void pairMessageMapping(@Header("simpSessionId") String sessionId, @Payload UserTopic userTopic) {
         UserPair userPair = pairingService.pair(sessionId, userTopic);
         if (userPair == null) {
             return;
         }
-        messagingTemplate.convertAndSend("/queue/greetings/" + userPair.getFirstUserId(), userPair.getSecondUserId());
-        messagingTemplate.convertAndSend("/queue/greetings/" + userPair.getSecondUserId(), userPair.getFirstUserId());
-    }
-
-    public void removeUser(String sessionId) {
-        sessionUserTopics.removeIf(sessionUserTopic -> sessionUserTopic.getSessionId().equals(sessionId));
+        messagingTemplate.convertAndSend("/queue/pair/" + userPair.getFirstUserId(), userPair.getSecondUserId());
+        messagingTemplate.convertAndSend("/queue/pair/" + userPair.getSecondUserId(), userPair.getFirstUserId());
     }
 }
